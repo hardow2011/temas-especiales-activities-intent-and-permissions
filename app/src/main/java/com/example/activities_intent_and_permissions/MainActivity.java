8f02a8da6e68c3fb57960c18623add9210bd2012
hardow2011 <hardow2011@outlook.com>
2021-09-30 07:38:33 -0400

    some testing

--- app/src/main/java/com/example/activities_intent_and_permissions/MainActivity.java
+++ app/src/main/java/com/example/activities_intent_and_permissions/MainActivity.java
@@ -1,8 +1,11 @@
 package com.example.activities_intent_and_permissions;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Switch;
 
 public class MainActivity extends AppCompatActivity {
@@ -24,5 +27,27 @@ public class MainActivity extends AppCompatActivity {
         phoneSwitch = findViewById(R.id.phoneSwitch);
         contactsSwitch = findViewById(R.id.contactsSwitch);
 
+        final String[] perms = {"android.permissions.FINE_LOCATION, android.permissions.CAMERA"};
+        final int permsRequestCode = 200;
+
     }
+
+//    @Override
+//    public void onClick(View v) {
+//        switch (v.getId())
+//    }
+
+
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        switch (requestCode) {
+            case 200:
+                boolean locationAndroidAccepted = grantResults[0] == PackageManager.PERMISSION_GRANTED;
+                boolean cameraAndroidAccepted = grantResults[1] == PackageManager.PERMISSION_GRANTED;
+                break;
+        }
+    }
+
 }
