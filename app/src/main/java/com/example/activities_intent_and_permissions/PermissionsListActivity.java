77d32939eb7b4a2e84694d60d0a397b06fed354a
hardow2011 <hardow2011@outlook.com>
2021-09-30 07:13:19 -0400

    Go back to Main activity button

---
+++ app/src/main/java/com/example/activities_intent_and_permissions/PermissionsListActivity.java
@@ -0,0 +1,14 @@
+package com.example.activities_intent_and_permissions;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PermissionsListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_permissions_list);
+    }
+}
