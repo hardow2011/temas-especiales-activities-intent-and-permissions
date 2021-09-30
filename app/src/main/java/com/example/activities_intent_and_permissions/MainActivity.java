0e6da7b3f6cfb4bb32eae02c7db31b4bbac301ce
hardow2011 <hardow2011@outlook.com>
2021-09-30 07:21:14 -0400

    Main activity components in java

--- app/src/main/java/com/example/activities_intent_and_permissions/MainActivity.java
+++ app/src/main/java/com/example/activities_intent_and_permissions/MainActivity.java
@@ -3,12 +3,26 @@ package com.example.activities_intent_and_permissions;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Switch;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Switch storageSwitch;
+    private Switch locationSwitch;
+    private Switch cameraSwitch;
+    private Switch phoneSwitch;
+    private Switch contactsSwitch;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        storageSwitch = findViewById(R.id.storageSwitch);
+        locationSwitch = findViewById(R.id.locationSwitch);
+        cameraSwitch = findViewById(R.id.cameraSwitch);
+        phoneSwitch = findViewById(R.id.phoneSwitch);
+        contactsSwitch = findViewById(R.id.contactsSwitch);
+
     }
 }
