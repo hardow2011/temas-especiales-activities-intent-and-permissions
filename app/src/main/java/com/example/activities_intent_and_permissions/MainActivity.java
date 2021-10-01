5e85b239e277c021af9a943cd555049240b00ae6
hardow2011 <hardow2011@outlook.com>
2021-09-30 22:50:02 -0400

    Permissions and jump to other activity

--- app/src/main/java/com/example/activities_intent_and_permissions/MainActivity.java
+++ app/src/main/java/com/example/activities_intent_and_permissions/MainActivity.java
@@ -2,11 +2,32 @@ package com.example.activities_intent_and_permissions;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
+import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Switch;
+import android.view.View.OnClickListener;
+import android.widget.Toast;
+
+
+import static android.Manifest.permission.READ_EXTERNAL_STORAGE;
+import static android.Manifest.permission.WRITE_EXTERNAL_STORAGE;
+import static android.Manifest.permission.ACCESS_FINE_LOCATION;
+import static android.Manifest.permission.CAMERA;
+import static android.Manifest.permission.CALL_PHONE;
+import static android.Manifest.permission.READ_CONTACTS;
+import static android.Manifest.permission.WRITE_CONTACTS;
+
+
+import java.sql.Array;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,6 +36,8 @@ public class MainActivity extends AppCompatActivity {
     private Switch cameraSwitch;
     private Switch phoneSwitch;
     private Switch contactsSwitch;
+
+    private final int PERMISSION_REQUEST_CODE = 200;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,26 +51,95 @@ public class MainActivity extends AppCompatActivity {
         contactsSwitch = findViewById(R.id.contactsSwitch);
 
         final String[] perms = {"android.permissions.FINE_LOCATION, android.permissions.CAMERA"};
-        final int permsRequestCode = 200;
 
     }
 
-//    @Override
-//    public void onClick(View v) {
-//        switch (v.getId())
-//    }
-
-
+    private void checkAndrequestPermissions(List<String> permissionList) {
+        String[] permissionArray = new String[permissionList.size()];
+        permissionArray = permissionList.toArray(permissionArray);
+        ActivityCompat.requestPermissions(this, permissionArray, 200);
+    }
 
     @Override
-    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
-        switch (requestCode) {
-            case 200:
-                boolean locationAndroidAccepted = grantResults[0] == PackageManager.PERMISSION_GRANTED;
-                boolean cameraAndroidAccepted = grantResults[1] == PackageManager.PERMISSION_GRANTED;
-                break;
+    public void onRequestPermissionsResult(int requestCode,
+                                           @NonNull String[] permissions,
+                                           @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode,
+                permissions,
+                grantResults);
+
+        Intent intent = new Intent(this, PermissionsListActivity.class);
+        startActivity(intent);
+    }
+
+//    @Override
+//    protected void onResume()
+//    {
+//        super.onResume();
+//        System.out.println("////////////////////////////////////////////////////");
+//        System.out.println("////////////////////////////////////////////////////");
+//        System.out.println("////////////////////////////////////////////////////");
+//        System.out.println("////////////////////////////////////////////////////");
+//        System.out.println("////////////////////////////////////////////////////");
+//        System.out.println("////////////////////////////////////////////////////");
+//        System.out.println("////////////////////////////////////////////////////");
+//        System.out.println("////////////////////////////////////////////////////");
+//    }
+
+    private void checkForGrantedPermissions() {
+        int storagePermission = ContextCompat.checkSelfPermission(getApplicationContext(), READ_EXTERNAL_STORAGE);
+        int locationPermission = ContextCompat.checkSelfPermission(getApplicationContext(), ACCESS_FINE_LOCATION);
+        int cameraPermission = ContextCompat.checkSelfPermission(getApplicationContext(), CAMERA);
+        int phonePermission = ContextCompat.checkSelfPermission(getApplicationContext(), CALL_PHONE);
+        int contactsPermission = ContextCompat.checkSelfPermission(getApplicationContext(), READ_CONTACTS);
+
+        if(storagePermission == PackageManager.PERMISSION_GRANTED) {
+            storageSwitch.setChecked(true);
+            storageSwitch.setEnabled(false);
         }
+        else {
+            storageSwitch.setChecked(false);
+            storageSwitch.setEnabled(true);
+        }
+    }
+
+//    @Override
+//    public void onAttachedToWindow() {
+//        super.onAttachedToWindow();
+//        Toast.makeText(this, "Hi UI is fully loaded", Toast.LENGTH_SHORT).show();
+//        System.out.println("////////////////////////////////////////////////////");
+//        System.out.println("////////////////////////////////////////////////////");
+//        System.out.println("////////////////////////////////////////////////////");
+//        System.out.println("////////////////////////////////////////////////////");
+//        System.out.println("////////////////////////////////////////////////////");
+//        System.out.println("////////////////////////////////////////////////////");
+//        System.out.println("////////////////////////////////////////////////////");
+//        System.out.println("////////////////////////////////////////////////////");
+//    }
+
+    public void sendInfo(View v) {
+        List<String > permissionList = new ArrayList<>();
+        if (storageSwitch.isChecked()) {
+            permissionList.add(READ_EXTERNAL_STORAGE);
+        }
+        if (locationSwitch.isChecked()) {
+            permissionList.add(ACCESS_FINE_LOCATION);
+        }
+        if (cameraSwitch.isChecked()) {
+            permissionList.add(CAMERA);
+        }
+        if (phoneSwitch.isChecked()) {
+            permissionList.add(CALL_PHONE);
+        }
+        if (contactsSwitch.isChecked()) {
+            permissionList.add(READ_CONTACTS);
+        }
+
+        if(permissionList.size() > 0) {
+            checkAndrequestPermissions(permissionList);
+        }
+
+
     }
 
 }
--- app/src/main/java/com/example/activities_intent_and_permissions/PermissionsListActivity.java
+++ app/src/main/java/com/example/activities_intent_and_permissions/PermissionsListActivity.java
@@ -2,6 +2,7 @@ package com.example.activities_intent_and_permissions;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 public class PermissionsListActivity extends AppCompatActivity {
@@ -10,5 +11,7 @@ public class PermissionsListActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_permissions_list);
+
+        Intent intent = getIntent();
     }
 }
